58ab02d9a38dfee49de9d68238bd3ade89fda477
Shpm21 <galleguilloscristian2@gmail.com>
2023-05-18 02:42:22 -0400

    feat: move to auth seccion to app/auth

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import Form from '../../components/Form';
+
+export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    console.log(email);
+
+    return (
+        <>
+            <Form />
+
+        </>
+    )
+}
---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "@/app/components/SignUpForm"
+
+const SignUp = () => {
+    return (
+        <>
+            <SignUpForm />
+        </>
+    )
+}
+
+export default SignUp
