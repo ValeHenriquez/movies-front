15e3ec15bcdc20f89d58b295ed168c8e9b8c9e63
Shpm21 <galleguilloscristian2@gmail.com>
2023-05-20 16:55:21 -0400

    feat: add my playlist page

---
+++ app/my-playlists/page.tsx
@@ -0,0 +1,13 @@
+import ShowPlaylists from "@/components/ShowPlaylists";
+
+const MyPlaylists = () => {
+
+    return <>
+        <div className="ml-10 mt-10">
+            <ShowPlaylists />
+        </div>
+    </>
+
+}
+
+export default MyPlaylists;
