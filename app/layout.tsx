1f6903e8c5a793f7702b9870e83fbf5ecb4f96c9
Shpm21 <galleguilloscristian2@gmail.com>
2023-05-20 16:55:37 -0400

    feat: add redux and tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@
 import { ApolloProvider } from '@apollo/client'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import client from './config/apollo-client'
+import client from '../config/apollo-client'
+import { ThemeProvider } from '@material-tailwind/react'
+import { Provider } from 'react-redux'
+import { store } from '@/store/store'
+import SideBar from '@/components/SideBar'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,15 +21,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const isBrowser = typeof window !== 'undefined';
+  const showSideBar = isBrowser && !window.location.pathname.includes('/login');
+
   return (
     <html lang="en"
       className="h-full bg-white"
     >
 
-      <body className='h-full'>
-        <ApolloProvider client={client}>
-          {children}
-        </ApolloProvider>
+      <body className='h-full flex flex row'>
+        <Provider store={store}>
+          <ThemeProvider>
+            <ApolloProvider client={client}>
+              {showSideBar && <SideBar />}
+              {children}
+            </ApolloProvider>
+          </ThemeProvider>
+        </Provider>
 
       </body>
     </html>
