e96eaf76a01dc7646aa314ccbcf5ffcf2ae33aa5
Vale_H <valentina.henriquez@alumnos.ucn.cl>
2023-05-20 15:58:55 -0400

    feat: cast page

---
+++ app/components/CastCard.tsx
@@ -0,0 +1,29 @@
+import { EMPTY_MOVIE_URL, IMAGE_URL } from "@/config";
+import Image from "next/image";
+import React from "react";
+import { Actor } from "../interfaces";
+
+
+const CastCard = ({ cast }: { cast: Actor }) => {
+    return (
+        <div className="w-full flex flex-col">
+            <div className="w-full h-[400px] relative">
+                <Image className="rounded-3xl shadow-lg"
+                    src={
+                        cast?.profile_path
+                            ? `${IMAGE_URL}${cast?.profile_path}`
+                            : `${EMPTY_MOVIE_URL}`
+                    }
+                    alt={cast?.name}
+                    fill={true}
+                />
+            </div>
+            <div className="flex flex-col gap-1 mt-3">
+                <h2 className="text-lg font-medium">{cast?.name}</h2>
+                <h2 className="text-lg font-medium">{cast?.character}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default CastCard;
