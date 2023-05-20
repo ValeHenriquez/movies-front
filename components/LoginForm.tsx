b1619b7aa3b8b751da4c4cb6c6709dc2286a907e
Vale_H <valentina.henriquez@alumnos.ucn.cl>
2023-05-20 18:51:42 -0400

    build: update reference

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -92,7 +92,7 @@ const LoginForm: React.FC = () => {
 
                 <p className="mt-10 text-center text-sm text-gray-500">
                     Not a member?{' '}
-                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                    <a href="/auth/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
                         Sing up
                     </a>
                 </p>
