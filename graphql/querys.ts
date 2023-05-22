8ebc8d2296fdb7dc9198333e23b8d25eb1d2a19e
Vale_H <valentina.henriquez@alumnos.ucn.cl>
2023-05-22 03:30:43 -0400

    build: add query get playlist

--- graphql/querys.ts
+++ graphql/querys.ts
@@ -14,7 +14,7 @@ export const GET_MOVIES = gql`
 export const GET_MOVIE_ID = gql`
 query getMovieByID($id: Int!) {
   getMovieByID(id: $id) {
-    id
+      id
       adult
       backdrop_path
       original_language
@@ -61,6 +61,42 @@ query profile {
     name
 		email
     password
+  }
+}
+`;
+
+
+export const GET_PLAYLIST_USER = gql`
+query playlistsByUser {
+  playlistsByUser {
+    id
+    title
+    description
+    movies{
+      id
+      adult
+      backdrop_path
+      original_language
+      original_title
+      overview
+      popularity
+      poster_path
+      release_date
+      title
+      video
+      vote_average
+      vote_count
+    actors{
+      id
+      name
+      character
+      profile_path
+    }
+    genres{
+      id
+      name
+    }
+    }
   }
 }
 `;
