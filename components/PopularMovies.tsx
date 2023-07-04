cefe3b6923bbf3bf9b1f89b1ecabe5a11f442f62
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-04 10:33:39 -0400

    feat: change text-color

--- components/PopularMovies.tsx
+++ components/PopularMovies.tsx
@@ -6,26 +6,26 @@ import { MovieShortInfo } from "../config/interfaces";
 const PopularMovies = ({ popularMovies }: { popularMovies: MovieShortInfo[] }) => {
     return (
         <div className="flex flex-col mb-6">
-            <div className="flex justify-between items-center mt-4">
+            <div className="flex items-center justify-between mt-4">
                 <h1 className="text-2xl font-medium">Popular Movies</h1>
                 <Link
                     href="/movies/popular"
-                    className="py-2 px-5 bg-slate-800 text-md font-normal text-white"
+                    className="px-5 py-2 font-normal text-white bg-slate-800 text-md"
                 >
                     See all
                 </Link>
             </div>
-            <div className="grid grid-cols-4 mt-4 gap-4">
+            <div className="grid grid-cols-4 gap-4 mt-4">
                 {popularMovies.slice(0, 4).map((movie: MovieShortInfo) => (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
             </div>
-            <div className="grid grid-cols-4 mt-4 gap-4">
+            <div className="grid grid-cols-4 gap-4 mt-4">
                 {popularMovies.slice(4, 8).map((movie: MovieShortInfo) => (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
             </div>
-            <div className="grid grid-cols-4 mt-4 gap-4">
+            <div className="grid grid-cols-4 gap-4 mt-4">
                 {popularMovies.slice(8, 12).map((movie: MovieShortInfo) => (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
