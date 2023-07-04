bfefbeea3058efa512fb5214f5f7a73ca8c29f8a
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-04 10:31:20 -0400

    feat: get to server component

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,55 +1,16 @@
-"use client"
-import { useLazyQuery, useQuery } from "@apollo/client";
-import { GET_MOVIES } from "../../graphql/querys";
-import MovieCard from "../../components/MovieCard";
-import { Movie, MovieShortInfo } from '../../config/interfaces';
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setMovies } from "@/store/slices/movieSlice";
-import { AppState } from "@/store/store";
-import Loading from "@/components/Loading";
-import { notFound } from "next/navigation";
+import { getClient } from "@/lib/client";
+import { ShowMovies } from "@/components/ShowMovies";
+import { GET_MOVIES_COUNT } from "@/graphql/querys";
 
-
-const Movies = () => {
-    const isAuthenticated = useSelector((state: AppState) => state.auth.isAuthenticated);
-    const [getMovies, { error, loading, data }] = useLazyQuery(GET_MOVIES);
-    const dispatch = useDispatch();
-    const popularMovies = useSelector((state: AppState) => state.movie.movies);
-    useEffect(() => {
-        getMovies();
-    }, [data]);
-
-    useEffect(() => {
-        if (data) {
-            dispatch(setMovies(data.movies as Movie[]));
-        }
-    }, [data]);
-    if (loading) {
-        return <Loading />
-    }
-    if (!isAuthenticated) {
-        notFound();
-    }
-
+const MoviesPage = async () => {
+    const data = await getClient().query<{ moviesCount: number }>({ query: GET_MOVIES_COUNT });
     return (
         <>
-            <main className="mt-5 flex flex-col">
-                <div className="max-w-full px-4 mx-auto">
-                    <div className="flex flex-col">
-                        <h1 className="text-2xl font-medium">Movies</h1>
-                    </div>
-                    <div className="grid grid-cols-5 mt-2 gap-2 
-                        overflow-y-scroll overflow-x-hidden max-h-[calc(100vh-4rem)]
-                    ">
-                        {popularMovies.map((movie: MovieShortInfo) => (
-                            <MovieCard key={movie?.id} movie={movie} />
-                        ))}
-                    </div>
-                </div>
-            </main>
+            <ShowMovies
+                moviesCount={data.data.moviesCount}
+            />
         </>
     );
 };
 
-export default Movies;
+export default MoviesPage;
