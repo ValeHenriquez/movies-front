19b254e8ded5e69d1f574114284028c9cb4bdd98
cgalleguillos-dev <galleguilloscristian2@gmail.com>
2023-07-04 10:32:22 -0400

    feat: change color text

--- components/CastCard.tsx
+++ components/CastCard.tsx
@@ -6,9 +6,9 @@ import { Actor } from "../config/interfaces"
 
 const CastCard = ({ cast }: { cast: Actor }) => {
     return (
-        <div className="w-full flex flex-col">
+        <div className="flex flex-col w-full">
             <div className="w-[200px] h-[200px] relative">
-                <Image className="rounded-3xl shadow-lg"
+                <Image className="shadow-lg rounded-3xl"
                     src={
                         cast?.profile_path
                             ? `${IMAGE_URL}${cast?.profile_path}`
@@ -19,8 +19,8 @@ const CastCard = ({ cast }: { cast: Actor }) => {
                 />
             </div>
             <div className="flex flex-col gap-1 mt-3">
-                <h2 className="text-lg font-medium">{cast?.name}</h2>
-                <h2 className="text-lg font-medium">{cast?.character}</h2>
+                <h2 className="text-lg font-medium text-white">{cast?.name}</h2>
+                <h2 className="text-lg font-medium text-white">{cast?.character}</h2>
             </div>
         </div>
     );
